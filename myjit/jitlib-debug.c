4c2a57ddb3651e95a0529f93b1ffbe8ff13ab3c4
pkrajca <pkrajca@users.sourceforge.net>
2010-09-19 23:00:56 +0200

    disassambler loads libopcodes dynamically and if the library is not present in the system, it tries to use old good gcc & objdump based disassambler.

--- myjit/jitlib-debug.c
+++ myjit/jitlib-debug.c
@@ -23,13 +23,13 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdarg.h>
+#include <dlfcn.h>
+#include <ctype.h>
 
 #include "llrb.c"
 
-#ifndef JIT_NODEBUG
 #include "../dis-asm/dis-asm.h"
 #include "../dis-asm/bfd.h"
-#endif
 
 #define OUTPUT_BUF_SIZE		(8192)
 
@@ -53,32 +53,89 @@ static void override_print_address(bfd_vma addr, struct disassemble_info *info)
 	sprintf(buf, "0x%lx", (long)addr);
 }
 
-void jit_dump_code(struct jit * jit, int verbosity)
+static void * get_libocodes_handle()
 {
-#ifndef JIT_NODEBUG
+	char buf[1024];
+
+	void * handle;
+	handle = dlopen("libopcodes.so", RTLD_LAZY);
+	if (handle) return handle;
+
+	FILE * f = popen("/sbin/ldconfig -p", "r");
+	while (f && !feof(f)) {
+		char * b;
+		fgets(buf, 1024, f);
+		b = strstr(buf, "libopcodes");
+		if (b) {
+			fclose(f);
+			for (int i = 0; i < strlen(b); i++) {
+				if (isspace(b[i])) {
+					b[i] = '\0';
+					break;
+				}
+			}
+			return dlopen(b, RTLD_LAZY);
+		}
+	}
+	fclose(f);
+	return NULL;
+}
+
+int opcodes_based_debugger(struct jit * jit)
+{
 	struct disassemble_info info;
 	unsigned long count, pc, buf_size;
 	char output_buf[OUTPUT_BUF_SIZE];
+	char * error;
+
+	void (* __init_disassemble_info) (struct disassemble_info *info, void *stream, fprintf_ftype fprintf_func);
+	int (* __print_insn) (bfd_vma, disassemble_info *);
+
+	void * handle = get_libocodes_handle();
+
+	if (!handle) {
+		fprintf(stderr, "Unable to find libopcodes library.\n");
+		return -1;
+	}
+
+	dlerror();    // Clear any existing error
+
+	*(void **) (&__init_disassemble_info) = dlsym(handle, "init_disassemble_info");
+
+	if ((error = dlerror()) != NULL)  {
+		fprintf(stderr, "%s\n", error);
+		return -1;
+	}
 
 	fprintf_ftype print_fn = (fprintf_ftype) override_printf;
-
-	init_disassemble_info(&info, output_buf, print_fn);
+	__init_disassemble_info(&info, output_buf, print_fn);
 	info.print_address_func = override_print_address;
 
 #ifdef JIT_ARCH_I386
 	info.mach = bfd_mach_i386_i386_intel_syntax;
 	info.endian = BFD_ENDIAN_LITTLE;
+
+	*(void **) (&__print_insn) = dlsym(handle, "print_insn_i386_intel");
 #endif
 
 #ifdef JIT_ARCH_AMD64
 	info.mach = bfd_mach_x86_64_intel_syntax;;
 	info.endian = BFD_ENDIAN_LITTLE;
+
+	*(void **) (&__print_insn) = dlsym(handle, "print_insn_x86_64_intel_syntax");
 #endif
 
 #ifdef JIT_ARCH_SPARC
 	info.mach = bfd_mach_sparc_sparclite;
 	info.endian = BFD_ENDIAN_BIG;
+
+	*(void **) (&__print_insn) = dlsym(handle, "print_insn_sparclite");
 #endif
+
+	if ((error = dlerror()) != NULL)  {
+		fprintf(stderr, "%s\n", error);
+		return -1;
+	}
 
 	buf_size = (unsigned long)(jit->ip - jit->buf);
 	info.buffer = (void *)jit->buf;
@@ -86,13 +143,44 @@ void jit_dump_code(struct jit * jit, int verbosity)
 
 	for (pc = 0; pc < buf_size; ) {
 		sprintf(output_buf, "%4lx:  ", pc);
-		count = print_insn_i386_intel(pc, &info);
+		count = __print_insn(pc, &info);
 		printf("%s\n", output_buf);
 		pc += count;
 	}
-#else
-	fprintf(stderr, "Internal debugger disabled. To turn this feature on, you have to recompile MyJIT without the JIT_NODEBUG directive.\n");
-#endif
+	return 0;
+}
+
+static void gcc_based_debugger(struct jit * jit)
+{
+	char * obj_file_name = tempnam(NULL, "myjit");
+
+	char * cmd1_fmt = "gcc -x assembler -c -o %s -";
+	char * cmd2_fmt = "objdump -d -M intel %s";
+
+	char cmd1[strlen(cmd1_fmt) + strlen(obj_file_name)];
+	char cmd2[strlen(cmd2_fmt) + strlen(obj_file_name)];
+
+	sprintf(cmd1, cmd1_fmt, obj_file_name);
+
+	FILE * f = popen(cmd1, "w");
+
+	int size = jit->ip - jit->buf;
+	fprintf (f, ".text\n.align 4\n.globl main\n.type main,@function\nmain:\n");
+	for (int i = 0; i < size; i++)
+		fprintf(f, ".byte %d\n", (unsigned int) jit->buf[i]);
+	fclose(f);
+
+	sprintf(cmd2, cmd2_fmt, obj_file_name);
+	system(cmd2);
+
+	unlink(obj_file_name);
+	free(obj_file_name);
+}
+
+void jit_dump_code(struct jit * jit, int verbosity)
+{
+	if (opcodes_based_debugger(jit))
+		gcc_based_debugger(jit);
 }
 
 char * jit_get_op_name(struct jit_op * op)
